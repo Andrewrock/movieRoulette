c8b846c61341d0e64a83eaa67f7e4abd870ef5a1
Andrew Rock <andrew@deepcovelabs.com>
2017-05-29 14:26:32 +0100

    fix routes for pages

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -52,7 +52,7 @@ gulp.task('deploy', function () {
 
 // Lint JavaScript
 gulp.task('lint', () =>
-  gulp.src(['app/scripts/**/*.js','!node_modules/**'])
+  gulp.src(['app/scripts/**/*.js','!app/scripts/angular.min.js','!app/scripts/angular-route.js','!node_modules/**'])
     .pipe($.eslint())
     .pipe($.eslint.format())
     .pipe($.if(!browserSync.active, $.eslint.failAfterError()))
@@ -127,7 +127,7 @@ gulp.task('scripts', () =>
       //       you need to explicitly list your scripts here in the right order
       //       to be correctly concatenated
       './app/scripts/angular.min.js',
-      '.app/scripts/angular-route.js',
+      './app/scripts/angular-route.js',
       './app/scripts/main.js'
       // Other scripts
     ])
@@ -152,7 +152,7 @@ gulp.task('html', () => {
   return gulp.src('app/**/*.html')
     .pipe($.useref({
       searchPath: '{.tmp,app}',
-      noAssets: true
+      noAssets: false
     }))
 
     // Minify any HTML
@@ -218,7 +218,7 @@ gulp.task('default', ['clean'], cb =>
   runSequence(
     'styles',
     ['lint', 'html', 'scripts', 'images', 'copy'],
-    'generate-service-worker',
+    //'generate-service-worker',
     cb
   )
 );
@@ -235,41 +235,41 @@ gulp.task('pagespeed', cb =>
 );
 
 // Copy over the scripts that are used in importScripts as part of the generate-service-worker task.
-gulp.task('copy-sw-scripts', () => {
-  return gulp.src(['node_modules/sw-toolbox/sw-toolbox.js', 'app/scripts/sw/runtime-caching.js'])
-    .pipe(gulp.dest('dist/scripts/sw'));
-});
+//gulp.task('copy-sw-scripts', () => {
+//  return gulp.src(['node_modules/sw-toolbox/sw-toolbox.js', 'app/scripts/sw/runtime-caching.js'])
+//    .pipe(gulp.dest('dist/scripts/sw'));
+//});
 
 // See http://www.html5rocks.com/en/tutorials/service-worker/introduction/ for
 // an in-depth explanation of what service workers are and why you should care.
 // Generate a service worker file that will provide offline functionality for
 // local resources. This should only be done for the 'dist' directory, to allow
 // live reload to work as expected when serving from the 'app' directory.
-gulp.task('generate-service-worker', ['copy-sw-scripts'], () => {
-  const rootDir = 'dist';
-  const filepath = path.join(rootDir, 'service-worker.js');
-
-  return swPrecache.write(filepath, {
-    // Used to avoid cache conflicts when serving on localhost.
-    cacheId: pkg.name || 'web-starter-kit',
-    // sw-toolbox.js needs to be listed first. It sets up methods used in runtime-caching.js.
-    importScripts: [
-      'scripts/sw/sw-toolbox.js',
-      'scripts/sw/runtime-caching.js'
-    ],
-    staticFileGlobs: [
-      // Add/remove glob patterns to match your directory setup.
-      `${rootDir}/images/**/*`,
-      `${rootDir}/scripts/**/*.js`,
-      `${rootDir}/styles/**/*.css`,
-      `${rootDir}/*.{html,json}`
-    ],
-    // Translates a static file path to the relative URL that it's served from.
-    // This is '/' rather than path.sep because the paths returned from
-    // glob always use '/'.
-    stripPrefix: rootDir + '/'
-  });
-});
+//gulp.task('generate-service-worker', ['copy-sw-scripts'], () => {
+//  const rootDir = 'dist';
+//  const filepath = path.join(rootDir, 'service-worker.js');
+//
+//  return swPrecache.write(filepath, {
+//    // Used to avoid cache conflicts when serving on localhost.
+//    cacheId: pkg.name || 'web-starter-kit',
+//    // sw-toolbox.js needs to be listed first. It sets up methods used in runtime-caching.js.
+//    importScripts: [
+//      'scripts/sw/sw-toolbox.js',
+//      'scripts/sw/runtime-caching.js'
+//    ],
+//    staticFileGlobs: [
+//      // Add/remove glob patterns to match your directory setup.
+//      `${rootDir}/images/**/*`,
+//      `${rootDir}/scripts/**/*.js`,
+//      `${rootDir}/styles/**/*.css`,
+//      `${rootDir}/*.{html,json}`
+//    ],
+//    // Translates a static file path to the relative URL that it's served from.
+//    // This is '/' rather than path.sep because the paths returned from
+//    // glob always use '/'.
+//    stripPrefix: rootDir + '/'
+//  });
+//});
 
 // Load custom tasks from the `tasks` directory
 // Run: `npm install --save-dev require-dir` from the command-line
